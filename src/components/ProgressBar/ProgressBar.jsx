0a2d368337f14b7c6440b783318a2fc086eace5a
Hubert <h.kwiatkowski@hotmail.com>
2023-05-04 19:40:59 +0200

    chore: progress bar

---
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -0,0 +1,11 @@
+import './ProgressBar.css'
+
+const ProgressBar = () => {
+  return (
+    <div className="progress-background">
+      <div className="progress"></div>
+    </div>
+  )
+}
+
+export default ProgressBar
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Navbar  from "./Navbar/Navbar.jsx";
-
+import ProgressBar from "./ProgressBar/ProgressBar.jsx";
 
 export {
-  Navbar
+  Navbar,
+  ProgressBar
 }
